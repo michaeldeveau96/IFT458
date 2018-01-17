0209c869e0128363c4e0daf0367e901fb878c520
unknown <mdeveau@asu.edu>
2018-01-16 23:56:23 -0700

    Layout update

--- PD1.js
+++ PD1.js
@@ -1,8 +1,8 @@
 function openNav() {
 	document.getElementById("sidebar_left").style.width = "250px";
-	document.getElementById("main").style.marginLeft = "250px";
+	document.getElementById("header").style.marginLeft = "310px";
 }
 function closeNav() {
 	document.getElementById("sidebar_left").style.width = "0px";
-	document.getElementById("main").style.marginLeft = "0px";
+	document.getElementById("header").style.marginLeft = "0px";
 }
