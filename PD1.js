46e4fdcf04ed867b5158107f66c1be0d542a5534
unknown <mdeveau@asu.edu>
2018-01-16 18:46:48 -0700

    Major ui update

--- PD1.js
+++ PD1.js
@@ -1,1 +1,8 @@
-function
+function openNav() {
+	document.getElementById("sidebar_left").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+}
+function closeNav() {
+	document.getElementById("sidebar_left").style.width = "0px";
+	document.getElementById("main").style.marginLeft = "0px";
+}
