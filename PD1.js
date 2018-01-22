a0bf5334a3c972e1badb898a102a889dfffa8b9b
unknown <mdeveau@asu.edu>
2018-01-22 13:09:49 -0700

    Major ui update

--- PD1.js
+++ PD1.js
@@ -1,18 +1,26 @@
 function openNav() {
 	document.getElementById("sidebar_left").style.width = "250px";
-	document.getElementById("header").style.marginLeft = "310px";
-	document.getElementById("header").style.width = "83.76%";	
-	document.getElementById("page").style.marginLeft = "310px";	
-	document.getElementById("page").style.width = "83.76%";
-	document.getElementById("sideNav-container").style.width = "310px";
 }
 function closeNav() {
 	document.getElementById("sidebar_left").style.width = "0px";
-	document.getElementById("SocialMediaLinks").style.marginLeft = "0px";
-	document.getElementById("header").style.marginLeft = "60px";
-	document.getElementById("header").style.width = "96.75%";
-	document.getElementById("page").style.width = "96.75%";
-	document.getElementById("page").style.marginLeft = "60px";
-	document.getElementById("sideNav-container").style.width = "60px";
+}
 
+var modal = document.getElementById('loginForm');
+
+function openLogin() {
+	document.getElementById('loginForm').style.display='block';
+}
+function closeLogin() {
+	document.getElementById('loginForm').style.display='none'
+}
+function openRegister() {
+	document.getElementById('registerForm').style.display='block';
+}
+function closeRegister() {
+	document.getElementById('registerForm').style.display='none'
+}
+window.onclick = function(event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
 }
